6d1d6e65648ef8a100b71d02eac7078b4e2d27d5
Edson Silva Ramos <eds.ramos@outlook.com>
2018-11-26 20:04:16 -0200

    Variáveis e operações Troco Frete

---
+++ AspNetVS2017.Capitulo01.VetoresColecoes.Teste/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AspNetVS2017.Capitulo01.VetoresColecoes.Teste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetVS2017.Capitulo01.VetoresColecoes.Teste")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a52ea1f-2f12-494e-9fff-bb221c456992")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AspNetVS2017.Capitulo01.VetoresColecoes.Teste/testcAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspNetVS2017.Capitulo01.VetoresColecoes.Teste
+{
+    internal class testcAttribute : Attribute
+    {
+    }
+}
