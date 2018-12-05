b87b7bd02a545b3d25748c6b8f5328757ff9ce83
Edson Silva Ramos <eds.ramos@outlook.com>
2018-12-04 22:37:45 -0200

    MVC

---
+++ AspNetVS2017.Capitulo03.Portfolio/Models/ContatoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AspNetVS2017.Capitulo03.Portfolio.Models
+{
+    public class ContatoViewModel
+    {
+
+        public int Id { get; set; }
+
+        [Required]        
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Mensagem { get; set; }
+    }
+}
