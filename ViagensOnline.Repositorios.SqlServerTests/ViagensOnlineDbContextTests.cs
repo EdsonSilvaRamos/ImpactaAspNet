ef575be7e1609742d171c13a8d36287e08e375cc
Edson Silva Ramos <eds.ramos@outlook.com>
2018-12-06 20:29:25 -0200

    Entity Framework

---
+++ ViagensOnline.Repositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViagensOnline.Repositorios.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.SqlServer.Tests
+{
+    [TestClass()]
+    public class ViagensOnlineDbContextTests
+    {
+        private readonly ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+
+        [TestMethod()]
+        public void InserirTeste()
+        {
+            var destino = new Destino();
+
+            destino.Cidade = "São Paulo";
+            destino.Nome = "Conheça São Paulo";
+            destino.NomeImagem = "Paulista.Png";
+            destino.Pais = "Brasil";
+
+            db.Destinos.Add(destino);
+
+            db.SaveChanges();
+        }
+    }
+}
