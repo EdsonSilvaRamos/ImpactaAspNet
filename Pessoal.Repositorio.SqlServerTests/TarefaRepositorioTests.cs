d96a68f24ab194730559620adb659c93ac67e9e8
Edson Silva Ramos <eds.ramos@outlook.com>
2018-12-10 22:33:44 -0200

    Acesso a dados ADO.Net

---
+++ Pessoal.Repositorio.SqlServerTests/TarefaRepositorioTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pessoal.Dominio.Entidades;
+using Pessoal.Repositorio.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoal.Repositorio.SqlServer.Tests
+{
+    [TestClass()]
+    public class TarefaRepositorioTests
+    {
+        private readonly TarefaRepositorio repositorio = new TarefaRepositorio();
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var tarefa = new Tarefa();
+
+            tarefa.Concluida = false;
+            tarefa.Nome = "Passar roupa";
+            tarefa.Observacoes = "Rápido";
+            tarefa.Prioridade = Prioridade.Alta;
+
+            tarefa.Id = repositorio.Inserir(tarefa);
+
+            Assert.AreNotEqual(tarefa.Id, 0);
+        }
+
+        [TestMethod()]
+        public void AtualizarTest()
+        {
+            var tarefa = new Tarefa();
+
+            tarefa.Id = 1;
+            tarefa.Concluida = true;
+            tarefa.Nome = "Lavar roupa no Sábado";
+            tarefa.Observacoes = "Baixa p/ Média";
+            tarefa.Prioridade = Prioridade.Baixa;
+
+            repositorio.Atualizar(tarefa);
+        }
+    }
+}
