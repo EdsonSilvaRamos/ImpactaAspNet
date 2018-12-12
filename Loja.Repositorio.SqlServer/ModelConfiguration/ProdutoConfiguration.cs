cd0c8af581f08326f850d97e6772799f7f640761
Edson Silva Ramos <eds.ramos@outlook.com>
2018-12-12 20:33:36 -0200

    Continuação Entity 12/12

---
+++ Loja.Repositorio.SqlServer/ModelConfiguration/ProdutoConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Loja.Dominio;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Loja.Repositorio.SqlServer.ModelConfiguration
+{
+    public class ProdutoConfiguration : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguration()
+        {
+            Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
